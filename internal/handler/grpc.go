82f1f0b44393a5a172d835be83cabc2157134437
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-02-05 20:12:54 +0100

    style: Add comments

--- internal/handler/grpc.go
+++ internal/handler/grpc.go
@@ -9,6 +9,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// A Handler defines the service's in-memory & controller
 type Handler struct {
 	gen.UnimplementedUserServiceServer
 	users map[string]user
@@ -19,34 +20,38 @@ type user struct {
 	Age  int32
 }
 
+// New instantiates and returns a new Handler.
 func New() *Handler {
-	return &Handler{}
+	return &Handler{users: make(map[string]user)}
 }
 
+// CreateUser inserts a new user into memory
 func (h *Handler) CreateUser(ctx context.Context, req *gen.CreateUserRequest) (*gen.CreateUserResponse, error) {
 	id := uuid.New().String()
 	h.users[id] = user{
-			Name: req.Name,
-			Age:  req.Age,
+		Name: req.Name,
+		Age:  req.Age,
 	}
 	return &gen.CreateUserResponse{Id: id}, nil
 }
 
+// ReadUser retrieves a user with from memory
 func (h *Handler) ReadUser(ctx context.Context, req *gen.ReadUserRequest) (*gen.ReadUserResponse, error) {
 	u, ok := h.users[req.Id]
 	if !ok {
-			return nil, status.Errorf(codes.NotFound, "user not found")
+		return nil, status.Errorf(codes.NotFound, "user not found")
 	}
 	return &gen.ReadUserResponse{
-			Name: u.Name,
-			Age:  u.Age,
+		Name: u.Name,
+		Age:  u.Age,
 	}, nil
 }
 
+// UpdateUser updates a user in memory
 func (h *Handler) UpdateUser(ctx context.Context, req *gen.UpdateUserRequest) (*gen.UpdateUserResponse, error) {
 	u, ok := h.users[req.Id]
 	if !ok {
-			return nil, status.Errorf(codes.NotFound, "user not found")
+		return nil, status.Errorf(codes.NotFound, "user not found")
 	}
 	u.Name = req.Name
 	u.Age = req.Age
@@ -54,6 +59,7 @@ func (h *Handler) UpdateUser(ctx context.Context, req *gen.UpdateUserRequest) (*
 	return &gen.UpdateUserResponse{}, nil
 }
 
+// DeleteUser deletes a user from memory.
 func (h *Handler) DeleteUser(ctx context.Context, req *gen.DeleteUserRequest) (*gen.DeleteUserResponse, error) {
 	delete(h.users, req.Id)
 	return &gen.DeleteUserResponse{}, nil
